3aa08436fb17e5d6cdbeb34a75fdcf332b93f648
Glasteroid <kennethangeles880@yahoo.com>
2024-03-22 13:41:14 -0700

    Update numpy_basics.py

--- numpy_basics.py
+++ numpy_basics.py
@@ -1,6 +1,6 @@
 # import numpy
-import numpy as np
 from numpy import *
+from numpy import random
 
 # initialize and array and print out the first element
 
@@ -26,7 +26,22 @@ b = arange(10)
 print(b)
 
 # initialize and print an array that print numbers between 0-10 with even increments of two
+# using linspace, both end points ARE included
+
 
 c = linspace(0, 10, 6)
 
 print(c)
+
+# initialize and print an array that print numbers between 0-10 with base e
+d = logspace(0, 10, 10, base=e)
+
+print(d)
+
+# initialize array with complex numbers data types using dtype
+M = array([[1, 2], [3, 4]], dtype=complex)
+
+print(M)
+
+# print 2d grid that prints out x-values between 0 and 3 uninclusive and y-values between 0 and 4 uninclusive
+print(mgrid[0:3, 0:4])
