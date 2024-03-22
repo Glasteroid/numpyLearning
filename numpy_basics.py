53d98c3096bef2aea21ea8ebd6c1cfc004f3731f
Glasteroid <kennethangeles880@yahoo.com>
2024-03-22 13:03:24 -0700

    Update numpy_basics.py

--- numpy_basics.py
+++ numpy_basics.py
@@ -1,23 +1,32 @@
 # import numpy
 import numpy as np
+from numpy import *
 
 # initialize and array and print out the first element
 
-a = np.array([1, 2, 3, 4, 5, 6])
+a = array([1, 2, 3, 4, 5, 6])
 
 print(a[0])
+
+# get shape of a
+print(a.shape)
+print(shape(a))
+
+# get size of a
+print(a.size)
+print(size(a))
 
 # number of dimensions of array
 print(a.ndim)
 
 # initialize and print an array that prints values from 0-9
 
-b = np.arange(10)
+b = arange(10)
 
 print(b)
 
 # initialize and print an array that print numbers between 0-10 with even increments of two
 
-c = np.linspace(0, 10, 6)
+c = linspace(0, 10, 6)
 
 print(c)
